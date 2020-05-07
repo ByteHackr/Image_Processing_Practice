069261e9e098b27d0aee4d682562c316ed0433d1
Sandipan Roy <sandipan@localhost.localdomain>
2020-05-07 09:08:05 +0530

    update_may_2020

--- CMFD/Functions/genDisk.m
+++
@@ -1,8 +0,0 @@
-function [D,n] = genDisk(radius)
-%generate a disk-shaped structuring element
-%  [D,n] = genDisk(radius)
-% 
-
-    [X,Y] = meshgrid(-radius:radius,-radius:radius);
-    D = double((X.^2+Y.^2)<=(radius^2));
-    n = sum(D(:));
